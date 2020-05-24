f7c1b797600c9aca26d6930efba9e5d2541c4ac4
Nathan Long <nrlong13@gmail.com>
2020-05-24 17:52:54 -0400

    stat trackers printing with graphs.  output on dashboard working

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workout = new Schema (
+const workoutSchema = new Schema (
     {
         day: {
             type: Date,
@@ -46,6 +46,12 @@ const workout = new Schema (
     }
 );
 
-const Workout = mongoose.model("Workout", workout);
+workoutSchema.virtual("totalDuration").get(function() {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+})
+
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
